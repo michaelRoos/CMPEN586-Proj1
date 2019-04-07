3c7fbeaae3254e1cf50c3cf3b3266519b9f498c7
Michael Roos <michael.d.roos@gmail.com>
2019-04-06 21:43:17 -0400

    added pose parsing functionality

--- main.m
+++ main.m
@@ -1,33 +1,6 @@
 pointCloud = inputPointCloud('Results/XYZ.csv');
 cameraIntrinsic = inputCameraIntrinsic('Results/CameraIntrinsic.csv');
+poses = inputPoses('Results/Pose.csv');
 
 plane = ransac(pointCloud);
 
-%# create x,y
-[xx,yy]=ndgrid(1:10,1:10);
-
-%# calculate corresponding z
-z = (-plane(1)*xx - plane(2)*yy - plane(4))/plane(3);
-
-%# plot the surface
-figure
-hold on
-surf(xx,yy,z)
-scatter3(pointCloud(:,1),pointCloud(:,2),pointCloud(:,3))
-hold off
-rotate3d on
-
-
-    # Create rotation matrix
-    quat = Quat((qw,qx,qy,qz))
-    rotMatrix = quat.transform()
-    # Create translation matrix of the following form
-    # | 0   -tz ty  |
-    # | tz  0   -tx |
-    # | -ty tx  0   |
-    tranMatrix = np.array([
-        [0,     -tz,    ty],
-        [tz,    0,      -tx],
-        [-ty,   tx,     0],
-    ])
-    essentialMatrix = np.matmul(rotMatrix,transMatrix)
